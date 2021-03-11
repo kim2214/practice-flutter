c1858ca40caa8c70ebc1d202bbd39b90f5b0d11f
Kyusoo.Kim <anzel21@naver.com>
2021-03-11 16:48:03 +0900

    add varibale comment

--- lib/marvel_movie.dart
+++ lib/marvel_movie.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class Marvel {
-  String imagePath;
-  String movieName;
-  String movietime;
+  String imagePath; // 영화 이미지 경로
+  String movieName; // 영화 제목
+  String movietime; // 영화 러닝타임
 
   Marvel(
     {@required this.movieName,
